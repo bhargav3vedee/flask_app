f1c96cab1218263cbc543aaf0fab790fcd96ef5a
bhargav3vedi <bhargav3vedi@gmail.com>
2025-01-14 19:01:45 +0530

    new route

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return "Hello world"
 
+@app.route('/index')
+def index():
+    return "Hello from index page"
+
 if __name__ == '__main__':
     app.run(debug=True)
-    
