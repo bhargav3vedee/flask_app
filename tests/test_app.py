576fcfef4f7db1da24ae04b68b516be7fa45d2d5
Bhargav Trivedi <bhargav3vedi@gmail.com>
2026-08-20 10:55:32 +0530

    Added new routes to flask app

---
+++ tests/test_app.py
@@ -0,0 +1,8 @@
+from main import app
+
+
+def test_hello():
+    with app.test_client() as client:
+        response = client.get('/')
+        assert response.status_code == 200
+        assert response.data == b"Hello, World!"
